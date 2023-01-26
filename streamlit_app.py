3d3c7d31f2a23bad96faba817c01a5dda158add7
horc82mx <123663788+horc82mx@users.noreply.github.com>
2023-01-26 15:45:51 -0600

    Update streamlit_app.py
    Added a function for choosing the Fruit Name Column as the Index

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,13 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 # streamlit.dataframe(my_fruit_list)
 
 # Let´s put a pick list here so they can pick the fruitthey want to include
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page
+# streamlit.dataframe(my_fruit_list)
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page
